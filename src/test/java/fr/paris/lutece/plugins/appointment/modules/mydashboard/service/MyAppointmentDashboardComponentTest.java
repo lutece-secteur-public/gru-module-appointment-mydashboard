583c0902bcc0e7ceb7cc9c95e733ffe3f2e89311
rdeniel <renaud.deniel@outlook.com>
2022-02-03 18:05:00 +0100

    APPMYDASH-7 add a unit test

---
+++ src/test/java/fr/paris/lutece/plugins/appointment/modules/mydashboard/service/MyAppointmentDashboardComponentTest.java
@@ -0,0 +1,14 @@
+package fr.paris.lutece.plugins.appointment.modules.mydashboard.service;
+
+import org.junit.Test;
+
+import fr.paris.lutece.test.LuteceTestCase;
+
+public class MyAppointmentDashboardComponentTest extends LuteceTestCase
+{
+    @Test
+    public void testMyAppointmentDashboardComponent( )
+    {
+        assertTrue( true );
+    }
+}
